19e1e5e7ec5de12016bc1d3c1c8640bb697c8208
shravanth-drife <shravanth@drife.io>
2022-09-09 12:57:13 +0530

    Initial commit

---
+++ cards/deck.go
@@ -0,0 +1,43 @@
+package main
+
+//Create a type of 'deck'
+// which is a slice of strings
+
+import (
+	"fmt"
+	"strings"
+)
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Clubs", "Hearts"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+
+}
---
+++ quiz/quiz.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	//var this laptopSize = 3.45
+	//fmt.Println(this.getSizeOfLaptop())
+	//this.getSizeOfLaptop()
+}
+
+type laptopSize float64
+
+func (this laptopSize) getSizeOfLaptop() laptopSize {
+	return this
+}
